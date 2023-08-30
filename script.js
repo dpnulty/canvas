b3cea607c61b1a5b8d3baf7e284bd517c619c25f
dpnulty <140291341+dpnulty@users.noreply.github.com>
2023-08-30 15:38:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function changeHeaderColor() {
     var header = document.querySelector('header');
     header.style.backgroundColor = '#f4f4f4'; // Change to your desired color
 }
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mobileMenuToggle = document.getElementById('mobile-menu');
+    var navList = document.getElementById('nav-list');
+
+    mobileMenuToggle.addEventListener('click', function() {
+        navList.classList.toggle('active');
+    });
+});
